229d7d9dc6e8102a7d2dbdf03d241b7eece1090f
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-12-07 12:34:42 -0500

    Upgrade gettext 0.26.2 => 1.0.2 (major)

--- apps/fire_starter_web/mix.exs
+++ apps/fire_starter_web/mix.exs
@@ -56,7 +56,7 @@ defmodule FireStarterWeb.MixProject do
        depth: 1},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
+      {:gettext, "~> 1.0"},
       {:fire_starter, in_umbrella: true},
       {:jason, "~> 1.2"},
       {:bandit, "~> 1.5"},
